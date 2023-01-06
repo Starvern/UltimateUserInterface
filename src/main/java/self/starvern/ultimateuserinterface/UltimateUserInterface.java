01a02dc0cab6cb433e1c1d4385f44110bfda757c
BuildTools <80870161+IIStarZ4@users.noreply.github.com>
2023-01-06 16:19:55 -0600

    Initial

---
+++ src/main/java/self/starvern/ultimateuserinterface/UltimateUserInterface.java
@@ -0,0 +1,19 @@
+package self.starvern.ultimateuserinterface;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class UltimateUserInterface extends JavaPlugin {
+
+    @Override
+    public void onEnable()
+    {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable()
+    {
+        // Plugin shutdown logic
+    }
+}
